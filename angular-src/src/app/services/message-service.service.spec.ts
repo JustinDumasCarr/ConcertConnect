d38e2499d6316b058d91cbafed47860e8002568d
alexandersokolov <alexander@kulapartners.com>
2017-11-19 16:40:05 -0400

    Message Service Fix
    Message Service Fix

--- angular-src/src/app/services/message-service.service.spec.ts
+++ angular-src/src/app/services/message-service.service.spec.ts
@@ -1,15 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { MessageServiceService } from './message-service.service';
+import { MessageService } from './message-service.service';
 
 describe('MessageServiceService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MessageServiceService]
+      providers: [MessageService]
     });
   });
 
-  it('should be created', inject([MessageServiceService], (service: MessageServiceService) => {
+  it('should be created', inject([MessageService], (service: MessageService) => {
     expect(service).toBeTruthy();
   }));
 });
