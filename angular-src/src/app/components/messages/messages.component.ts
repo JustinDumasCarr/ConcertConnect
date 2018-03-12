c2eba434aa0cdda2b805573d94d433f631ab3b01
alexandersokolov <alexander@kulapartners.com>
2018-03-12 17:24:07 -0300

    Artist Request
    -

--- angular-src/src/app/components/messages/messages.component.ts
+++ angular-src/src/app/components/messages/messages.component.ts
@@ -12,6 +12,8 @@ export class MessagesComponent implements OnInit {
 
 
   inboxEmpty: boolean;
+  requestData: any;
+  currentRequest: any;
 
   constructor(private authService: AuthService) {
 
@@ -19,31 +21,38 @@ export class MessagesComponent implements OnInit {
 
   ngOnInit() {
 
-    this.inboxEmpty = false;
+    this.inboxEmpty = true;
 
     // Currently only checks for incoming requests, add messages later
     let currentUser = JSON.parse(this.authService.getActiveLocal());
-
-    console.log(currentUser);
-    console.log("Current user id testing");
-    console.log(currentUser['_id']);
-
 
     // Check if current user is artist or venue
     if(currentUser['type'] == 'artist') {
         let artist = { artistId: currentUser['_id'], artistName: currentUser['name']};
         this.authService.getCurrentRequestsArtist(artist).subscribe(response => {
-          console.log("Response");
-          console.log(response);
           if(response['requestData'].length > 0) {
-            console.log("There is at least one request");
+            this.inboxEmpty = false;
+            this.requestData = response['requestData'];
+            this.currentRequest = this.requestData[0];
           }
         });
     } else if(currentUser['type'] == 'venue') {
-
+      // Fetch venue request here the same way artist was done
     } else {
       // Not sure if normal user is going to have messages
     }
+  }
+
+  setCurrentRequest(request) {
+    this.currentRequest = request;
+  }
+
+  acceptRequest() {
+    // Delete request and create contract
+  }
+
+  declineRequest() {
+    // Delete request
   }
 
 }
